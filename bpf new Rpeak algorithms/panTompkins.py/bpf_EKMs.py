44778c3a73cb74b61e94df549254bc67b6755ed5
Amirhossein Safari <97463137as@gmail.com>
2024-11-02 21:02:13 +0330

    Added the base code for reading the file data and applying functions files into them (same for all the algorithms)

---
+++ bpf new Rpeak algorithms/panTompkins.py/bpf_EKMs.py
@@ -0,0 +1,93 @@
+########################################
+#       Installation and Imports
+########################################
+
+from ishneholterlib import Holter
+import os
+# len(os.listdir("./ecg_200"))
+# from user_ekm_functions import user_ekm_dataset
+from user_ekms_functions import user_ekm_dataset
+import multiprocessing
+
+########################################
+#           Initial variables
+########################################
+
+dataset_path = "./dataset/ECG_200"
+users_files = os.listdir(dataset_path)
+users_files.remove("clinicalData-selected")
+
+# Initialization of dataset extracting processing
+dataset_name = "main_ekm_dataset"
+base_ekms_path = f'EKM_dataset'
+
+lead_names_dict = {
+    1: "x_lead",
+    2: "y_lead",
+    3: "z_lead"
+}
+
+########################################
+#       Reading files and playground!
+########################################
+
+# Getting .ecg files of users
+users_ecg_files = []
+for _file in users_files:
+  f_extention = _file.split(".")[1]
+  if f_extention == "ecg":
+    users_ecg_files.append(_file)
+
+# Removing users that their EKMs have been extracted
+user_ekms_zip_files = os.listdir("./Users EKM zip")
+list_of_ekm_extracted_users = []
+
+for _file in user_ekms_zip_files:
+  try:
+    extention = _file.split(".")[1]
+    if extention == "zip":
+      user_id = _file.split(".")[0]
+      list_of_ekm_extracted_users.append(user_id)
+  except:
+    pass
+
+for usr in list_of_ekm_extracted_users:
+  try:
+    users_ecg_files.remove(usr + ".ecg")
+  except:
+    print(f"User No. {usr} already been removed.")
+
+########################################
+#            Multi processing
+########################################
+
+# Specify the number of processes in the pool
+num_processes = multiprocessing.cpu_count()
+
+# Creating slices of users' ecg file for multiprocessing
+slices_size = num_processes
+number_of_complete_slices = len(users_ecg_files)//slices_size
+users_ecg_files_chunks = [users_ecg_files[_ * slices_size: (_+1) * slices_size] for _ in range(number_of_complete_slices)]
+
+if number_of_complete_slices * slices_size != len(users_ecg_files):
+    users_ecg_files_chunks.append(users_ecg_files[number_of_complete_slices * slices_size:])
+
+
+def processing_ecg_files(users_ecg_files_chunk):
+    with multiprocessing.Manager() as manager:
+        # Create a shared counter
+        shared_counter = manager.Value('i', 0)
+
+        # Create a lock from the manager
+        lock = manager.Lock()
+
+        # Create a pool of processes
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            # Pass the shared counter, lock, and total number of elements to the worker function
+            pool.starmap(user_ekm_dataset, [(user, shared_counter, lock, len(users_ecg_files_chunk)) for user in users_ecg_files_chunk])
+
+for users_ecg_files_chunk in users_ecg_files_chunks:
+    processing_ecg_files(users_ecg_files_chunk)
+
+# Print final progress
+print("Processing complete.")
